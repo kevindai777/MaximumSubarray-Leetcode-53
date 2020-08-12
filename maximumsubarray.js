b77bc6cacb2e21bbf070af3fc92818a4ab3ef644
Kevin Dai <66235906+kevindai777@users.noreply.github.com>
2020-08-12 15:34:32 -0400

    Update maximumsubarray.js

--- maximumsubarray.js
+++ maximumsubarray.js
@@ -16,3 +16,18 @@ for (let i = 1; i < nums.length; i++) {
 }
 
 return endMax
+
+
+//O(n) solution that uses Kadane's Algorithm
+
+let currSum = 0
+let endSum = -Infinity
+
+for (let num of nums) {
+    currSum += num
+    endSum = Math.max(endSum, currSum)
+    //If it's a negative prefix sum, forget about it and reset
+    currSum = Math.max(0, currSum)
+}
+
+return endSum
